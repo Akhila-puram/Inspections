4c1745e6fee03a7dca1b4f69c210b799153c6996
Akhila Puram <Akhila.Puram@aaseya.com>
2025-08-04 15:42:37 +0530

    added whole Inspections application

---
+++ src/main/java/com/aaseya/AIS/OMSConstant.java
@@ -0,0 +1,34 @@
+package com.aaseya.AIS;
+ 
+public class OMSConstant {
+	  
+	 public static final String TASKS_ASSIGN_URL = "https://ont-1.tasklist.camunda.io/c155667a-8cf3-4c88-b8a7-0564dbacb0f2/v1/tasks/";
+	    public static String CLIENT_ID = "6_lEjFrhppI8FCJ_fB9J5dbRHFJSe6YY";
+	    public static String TOKEN_URL = "https://login.cloud.camunda.io/oauth/token";
+	    public static String CLIENT_SECRET = "yqTc8mYC7gsEVGqXLKTnZ1N~0m.lLtHhxBsg53TdxAcH1dbOGxeMaryQE8t3qe3M";
+	    public static String GRANT_TYPE = "client_credentials";
+	    public static String TASKS_SEARCH_URL = "https://ont-1.tasklist.camunda.io/c155667a-8cf3-4c88-b8a7-0564dbacb0f2/v1/tasks/search";
+	    public static String BASE_TASK_URL = "https://ont-1.tasklist.camunda.io/c155667a-8cf3-4c88-b8a7-0564dbacb0f2/v1/tasks/";
+	    public static String COMPLETE_URL = "/complete";
+	    public static String SEARCH_TASK_VARIABLES = "/variables/search";
+	    public static String GET_TASK_URL = "https://ont-1.tasklist.camunda.io/c155667a-8cf3-4c88-b8a7-0564dbacb0f2/v1/tasks/";
+	    public static String GET_FORM_URL = "https://ont-1.tasklist.camunda.io/c155667a-8cf3-4c88-b8a7-0564dbacb0f2/v1/forms/"; // Add this line
+	    public static String KEYCLOAK_CLIENT_ID = "aaseyainspectionsolution";
+	    public static String KEYCLOAK_CLIENT_SECRET = "nTJJR3VDiLS0GQGySp2aBcNUUHRQIUVn";
+	    public static String AUDIENCE = "tasklist.camunda.io";
+//  
+
+//	public static final String TASKS_ASSIGN_URL = "http://10.13.1.180:8082/v1/tasks/";
+//	public static String CLIENT_ID = "tasklist";
+//	public static String TOKEN_URL = "http://10.13.1.180:18080/auth/realms/camunda-platform/protocol/openid-connect/token";
+//	public static String CLIENT_SECRET = "XALaRPl5qwTEItdwCMiPS62nVpKs7dL7";
+//	public static String GRANT_TYPE = "client_credentials";
+//	public static String TASKS_SEARCH_URL = "http://10.13.1.180:8082/v1/tasks/search";
+//	public static String BASE_TASK_URL = "http://10.13.1.180:8082/v1/tasks/";
+//	public static String COMPLETE_URL = "/complete";
+//	public static String SEARCH_TASK_VARIABLES = "/variables/search";
+//	public static String GET_TASK_URL = "http://10.13.1.180:8082/v1/tasks/";
+//	public static String GET_FORM_URL = "http://10.13.1.180:8082/v1/forms/";
+//	public static String KEYCLOAK_CLIENT_ID = "aaseyainspectionsolution";
+//	public static String KEYCLOAK_CLIENT_SECRET = "nTJJR3VDiLS0GQGySp2aBcNUUHRQIUVn";
+}
